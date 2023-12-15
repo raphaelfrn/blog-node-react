a4b0fb265458f8e18b503930080c4becb06b3981
Raphaël <raphfar@gmail.com>
2023-12-15 15:08:03 +0100

    fixed index js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import postRoutes from "./routes/posts.js"
-import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -37,10 +36,7 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-
-
 app.listen(8800,()=>{
     console.log('Connected');
 })
